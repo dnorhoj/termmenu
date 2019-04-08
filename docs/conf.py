2c575eea42f0a1c7d04a21cb46cd4304058cc14f
dnorhoj <11206485+dnorhoj@users.noreply.github.com>
2019-04-08 21:25:06 +0200

    lasttry

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,7 @@ author = 'dnorhoj'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
