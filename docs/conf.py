1cc4f44a4b82891832e31f57b94d664f192318ae
dnorhoj <11206485+dnorhoj@users.noreply.github.com>
2019-04-08 21:13:46 +0200

    idk what im doing

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@ import sys
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 import os, sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
