c33583ff378a03a21d7ea07dba54e064c546d76e
Daniel Nielsen <11206485+dnorhoj@users.noreply.github.com>
2019-04-09 10:19:34 +0200

    I think i finally got it

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,10 @@ author = 'dnorhoj'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc"
 ]
+
+master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
