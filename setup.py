aac9627ed02325a83e9a1cd140bc523a69f299f0
Daniel Nielsen <11206485+dnorhoj@users.noreply.github.com>
2019-05-21 09:43:40 +0200

    wrong github url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     description="A tool for making terminal menus.",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/dnorhoj/termmenus",
+    url="https://github.com/dnorhoj/termmenu",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
