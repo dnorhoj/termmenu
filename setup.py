20665c1c085c9a750ef1859e497080b2cf1b5e05
Daniel Nielsen <11206485+dnorhoj@users.noreply.github.com>
2019-05-21 08:46:05 +0200

    should work now

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="termmenu",
-    version="1.0",
+    version="1.1",
     author="dnorhoj",
     author_email="daniel.norhoj@gmail.com",
     description="A tool for making terminal menus.",
--- termmenu/__init__.py
+++ termmenu/__init__.py
@@ -1,1 +1,2 @@
+from termmenu import *
 name = "termmenu"
